7f220762f08eadcde74ae78408e4a22afe10fef6
Robert R. Russell <robert@rrbrussell.com>
2023-12-26 17:43:18 -0600

    This works for create a log message.
    Now, I need to experiment with pushing the logging out to a separate thread. After that I need to experiment with sleeping for a future time delay.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::process::Command;
 fn main() {
-    println!("Hello, world!");
+    println!("Spawning a thread to log something process");
+    let mut output = Command::new("logger")
+        .arg("I logged something.")
+        .spawn()
+        .expect("Find the logger command.");
+    match output.wait() {
+        Err(e) => {
+            println!("Logger failed with {e}")
+        }
+        Ok(out) => {
+            println!("Logger exited with {out}")
+        }
+    }
 }
