5a021af72ae0ed90ebf53481abc991f7ca6fe6fe
Robert R. Russell <robert@rrbrussell.com>
2023-12-29 16:44:10 -0600

    The most basic use of the std::thread API is implemented.
    This closes #1.
    
    Next on the todo list is to test thread sleeping.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 use std::process::Command;
+use std::thread;
 fn main() {
-    println!("Spawning a thread to log something process");
-    let mut output = Command::new("logger")
-        .arg("I logged something.")
-        .spawn()
-        .expect("Find the logger command.");
-    match output.wait() {
-        Err(e) => {
-            println!("Logger failed with {e}")
+    println!("Spawning a thread to log something on.");
+    let thread_join_handle = thread::spawn(move || {
+        println!("Spawning a process to log something.");
+        let mut output = Command::new("logger")
+            .arg("I logged something.")
+            .spawn()
+            .expect("Find the logger command.");
+        match output.wait() {
+            Err(e) => {
+                println!("Logger failed with {e}")
+            }
+            Ok(out) => {
+                println!("Logger exited with {out}")
+            }
         }
-        Ok(out) => {
-            println!("Logger exited with {out}")
+    } );
+    let result = thread_join_handle.join();
+    match result {
+        Err(_) => {
+            println!("The thread did not work.")
+        }
+        Ok(_) => {
+            println!("The thread did work.\nPlease check /var/log/messages.")
         }
     }
 }
